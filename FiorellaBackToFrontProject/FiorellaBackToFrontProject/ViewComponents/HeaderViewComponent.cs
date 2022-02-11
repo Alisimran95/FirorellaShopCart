83a87bc8552ab917a967b1555befca6ec700738b
Alisimran95 <alisimranem@code.edu.az>
2022-02-11 13:48:02 +0400

    homework

---
+++ FiorellaBackToFrontProject/FiorellaBackToFrontProject/ViewComponents/HeaderViewComponent.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FiorellaBackToFrontProject.Controllers;
+using FiorellaBackToFrontProject.DataAccessLayer;
+using FiorellaBackToFrontProject.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+
+namespace FiorellaBackToFrontProject.ViewComponents
+{
+    public class HeaderViewComponent : ViewComponent
+    {
+        private readonly AppDbContext _dbContext;
+
+        public HeaderViewComponent(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var count = 0;
+            var basket = Request.Cookies["basket"];
+           
+            if (!string.IsNullOrEmpty(basket))
+            {
+                var products = JsonConvert.DeserializeObject<List<BasketViewModel>>(basket);
+                count = products.Count;
+            }
+            ViewBag.BasketCount = count;
+            //var products2 = JsonConvert.DeserializeObject<List<BasketViewModel>>(basket);
+            //double totalAmount = 0;
+
+            //foreach (var amount in products2)
+            //{
+            //    totalAmount += amount.Count * amount.Price;
+            //    ViewBag.totalamount = totalAmount;
+            //}
+
+            var bio = await _dbContext.Bios.SingleOrDefaultAsync();
+            return View(bio);
+        }
+    }
+}
